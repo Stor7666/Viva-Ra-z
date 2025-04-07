9c7c2de1660bcc64380ea95af1bcc2dc27534053
nodeo-p <charlespj4647@gmail.com>
2025-04-06 19:58:38 -0600

    Create tailwind,config.js
    Estilos y paletas de colores

---
+++ tailwind,config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'blue-light': '#85C1FF',  // Azul claro
+        'blue-dark': '#1E90FF',   // Azul oscuro
+        'blue-darker': '#0D47A1',  // Azul más oscuro
+      },
+      fontFamily: {
+        sans: ['Helvetica Neue', 'Arial', 'sans-serif'], // Fuentes contemporáneas
+        serif: ['Georgia', 'serif'],
+        mono: ['Courier New', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
