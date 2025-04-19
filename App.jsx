aeeb174c0b5e0809b7284e9aaafa72e63ad6c8ca
nodeo-p <charlespj4647@gmail.com>
2025-04-18 22:08:36 -0600

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,43 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import PropertyDetails from './pages/PropertyDetails';
-import CompanyDetails from './pages/CompanyDetails';
-import SearchResults from './pages/SearchResults';
-import Plans from './pages/Plans';
-import UploadProperty from './pages/UploadProperty';
-import UploadCompany from './pages/UploadCompany';
-import Profile from './pages/Profile';
-import NotFound from './pages/NotFound';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Home from './pages/Home';
+import Explore from './pages/Explore';
+import Publish from './pages/Publish';
+import Services from './pages/Services';
+import Projects from './pages/Projects';
+import About from './pages/About';
 
 const App = () => {
   return (
     <Router>
       <Navbar />
-      <div className="container mx-auto p-4">
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/property/:id" component={PropertyDetails} />
-          <Route path="/company/:id" component={CompanyDetails} />
-          <Route path="/search" component={SearchResults} />
-          <Route path="/plans" component={Plans} />
-          <Route path="/upload-property" component={UploadProperty} />
-          <Route path="/upload-company" component={UploadCompany} />
-          <Route path="/profile" component={Profile} />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/publish" element={<Publish />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
       <Footer />
     </Router>
   );
 };
 
 export default App;
+
